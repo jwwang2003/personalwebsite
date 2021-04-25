353df56c75e1160bc5fa48abd03ad62758e5c0f7
Jun Wei Wang <jwwang.2003@gmail.com>
2021-04-24 17:12:57 -0700

    tested build

--- webpack.config.js
+++ webpack.config.js
@@ -58,31 +58,11 @@ module.exports = {
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
       minRatio: 0.8
-      })
+      }),
+    new BundleAnalyzerPlugin()
   ],
   devServer: {
     hot: true,
     historyApiFallback: true
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
   },
 }
