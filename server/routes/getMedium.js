3146a49245d049d1beef0fd771d8076995e8be25
Jimmy Wang <jwwang.2003@gmail.com>
2021-07-18 17:31:17 -0700

    cleaned up server code

--- server/routes/getMedium.js
+++ server/routes/getMedium.js
@@ -1,16 +1,17 @@
-import request from 'request'
-import Parser from 'rss-parser'
-const parser = new Parser()
+import axios from 'axios'
 
-export default function getMedium(req, res, next) {
-  request(
-    { url: 'https://medium.com/feed/@jwwang03' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message:
-      err.message })
-    }
-    res.set('Content-Type', 'application/json')
-    parser.parseString(body).then(parsed => res.send(parsed))
-  })
+const mediumUser = '@jwwang03'
+const rssToJsonApi = 'https://api.rss2json.com/v1/api.json'
+
+export default async function getMedium(req, res, next) {
+  const mediumRssFeed = `https://medium.com/feed/${mediumUser}`
+  const data = { params: { rss_url: mediumRssFeed } }
+  const rss = await axios.get(rssToJsonApi, data)
+
+  // debug
+  // console.log(rss.data)
+
+  res.set('Content-Type', 'application/json')
+  return res.send(rss.data)
+  // return parser.parseString(rss).then((parsed) => res.send(parsed))
 }
